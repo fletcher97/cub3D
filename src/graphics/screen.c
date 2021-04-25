80ec24014d954c6106206da7a1c27d822e6dafe3
Miguel Gueifao <miguel.gueifao@gmail.com>
2021-04-25 02:58:03 +0100

    Changed buffer stratagy to reuse old img

--- src/graphics/screen.c
+++ src/graphics/screen.c
@@ -6,7 +6,7 @@
 /*   By: mgueifao <mgueifao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/06 18:55:16 by mgueifao          #+#    #+#             */
-/*   Updated: 2021/03/09 01:23:12 by mgueifao         ###   ########.fr       */
+/*   Updated: 2021/04/25 00:08:32 by mgueifao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,15 +29,13 @@ void	my_mlx_pixel_put(t_img *img, int x, int y, int color)
 	*(unsigned int*)dst = color;
 }
 
-void	next_frame(t_cub3d cub3d)
+void	next_frame(t_screen screen, void *mlx)
 {
-	// mlx_clear_window(cub3d.mlx, cub3d.screen.win);
-	mlx_put_image_to_window(cub3d.mlx, cub3d.screen.win,
-		cub3d.img[1].img, 0, 0);
-	mlx_destroy_image(cub3d.mlx, cub3d.img[0].img);
-	cub3d.img[0] = cub3d.img[1];
-	cub3d.img[1].img =
-		mlx_new_image(cub3d.mlx, cub3d.screen.width, cub3d.screen.height);
-	cub3d.img[1].addr = mlx_get_data_addr(cub3d.img[1].img,
-		&cub3d.img[1].bpp, &cub3d.img[1].line, &cub3d.img[1].endian);
+	t_img tmp;
+
+	mlx_put_image_to_window(mlx, screen.win,
+		screen.img[1].img, 0, 0);
+	tmp = screen.img[0];
+	screen.img[0] = screen.img[1];
+	screen.img[1] = tmp;
 }
