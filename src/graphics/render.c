36a7527144410ea317e3abfe16587a5f4ce88fd5
Miguel Gueifao <miguel.gueifao@gmail.com>
2021-04-25 02:57:31 +0100

    Replaced for loops with while

--- src/graphics/render.c
+++ src/graphics/render.c
@@ -6,33 +6,60 @@
 /*   By: mgueifao <mgueifao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/09 00:21:57 by mgueifao          #+#    #+#             */
-/*   Updated: 2021/03/09 02:07:07 by mgueifao         ###   ########.fr       */
+/*   Updated: 2021/04/25 02:54:52 by mgueifao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "cub3d.h"
+#include <math.h>
+
 #include "mlx.h"
+#include "ft_stdlib.h"
 
-static void	render_ceil(t_cub3d cub3d, int color)
-{
-	int c = mlx_get_color_value(cub3d.mlx, color);
-	for (int i = 0; i < cub3d.screen.height / 2; i++)
-		for (int j = 0; j < cub3d.screen.width; j++)
-			my_mlx_pixel_put(&cub3d.img[1], j, i, c);
-}
+#include "cub3d.h"
+#include "c3d_graphics.h"
 
-static void	render_floor(t_cub3d cub3d, int color)
+static void	render_ceil(t_screen screen, int color, void *mlx)
 {
+	int i;
+	int j;
 	int c;
 
-	c = mlx_get_color_value(cub3d.mlx, color);
-	for (int i = cub3d.screen.height / 2; i < cub3d.screen.height; i++)
-		for (int j = 0; j < cub3d.screen.width; j++)
-			my_mlx_pixel_put(&cub3d.img[1], j, i, c);
+	c = mlx_get_color_value(mlx, color);
+	i = -1;
+	while (++i < screen.height / 2)
+	{
+		j = -1;
+		while (++j < screen.width)
+			my_mlx_pixel_put(&screen.img[1], j, i, c);
+	}
+}
+
+static void	render_floor(t_screen screen, int color, void *mlx)
+{
+	int i;
+	int j;
+	int c;
+
+	c = mlx_get_color_value(mlx, color);
+	i = screen.height / 2 - 1;
+	while (++i < screen.height)
+	{
+		j = 0;
+		while (++j < screen.width)
+			my_mlx_pixel_put(&screen.img[1], j, i, c);
+	}
+}
+
+static void	render_wall(t_player player, t_map map, t_screen screen)
+{
+	(void) player;
+	(void) map;
+	(void) screen;
 }
 
 void render(t_cub3d cub3d)
 {
-	render_ceil(cub3d, 0x00ff0000);
-	render_floor(cub3d, 0x0000ff00);
+	render_ceil(cub3d.screen, 0x000000ff, cub3d.mlx);
+	render_floor(cub3d.screen, 0x00bbff55, cub3d.mlx);
+	render_wall(cub3d.game.player, cub3d.game.map, cub3d.screen);
 }
