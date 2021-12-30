867ceffac871d2f853cc2ed55666117a987a5c74
Miguel Gueifao <miguel.gueifao@gmail.com>
2021-12-30 00:33:24 +0000

    Changed cub var to be a pointer

--- src/listeners/key_listener.c
+++ src/listeners/key_listener.c
@@ -6,7 +6,7 @@
 /*   By: mgueifao <mgueifao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/25 07:24:09 by mgueifao          #+#    #+#             */
-/*   Updated: 2021/12/30 00:06:58 by mgueifao         ###   ########.fr       */
+/*   Updated: 2021/12/30 00:32:39 by mgueifao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,22 @@
 
 int	stop(void *param)
 {
-	t_cub3d	cub3d;
+	t_cub3d	*cub3d;
 
-	cub3d = *((t_cub3d *) param);
+	cub3d = ((t_cub3d *) param);
 	terminate(cub3d, 0);
+	return (0);
 }
 
 int	on_key_press(int key_code, void *param)
 {
-	t_cub3d	cub3d;
+	t_cub3d	*cub3d;
 
-	cub3d = *((t_cub3d *) param);
+	cub3d = ((t_cub3d *) param);
 	if (key_code == C3D_KEY_ESC)
 		terminate(cub3d);
 	printf("%d\n", key_code);
+	return (1);
 }
 
 int	on_key_release(int key_code, void *param)
