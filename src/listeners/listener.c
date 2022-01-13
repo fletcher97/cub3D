704fff86b65da88968a39db76ed66e219ad489ba
Miguel Gueifao <miguel.gueifao@gmail.com>
2022-01-13 23:45:07 +0000

    Fixed passing wrong param to listeners

--- src/listeners/listener.c
+++ src/listeners/listener.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   listener.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jsantos <jsantos@student.42.fr>            +#+  +:+       +#+        */
+/*   By: mgueifao <mgueifao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/29 23:01:15 by mgueifao          #+#    #+#             */
-/*   Updated: 2022/01/11 18:31:33 by jsantos          ###   ########.fr       */
+/*   Updated: 2022/01/13 23:37:41 by mgueifao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@
 
 void	set_listeners(t_cub3d *cub3d)
 {
-	mlx_hook(cub3d->screen.win, MLX_DESTROY_NOTIFY, 1L, stop, &cub3d);
-	mlx_hook(cub3d->screen.win, MLX_KEY_PRESS, (1L << 0), on_key_press, &cub3d);
+	mlx_hook(cub3d->screen.win, MLX_DESTROY_NOTIFY, 1L, stop, cub3d);
+	mlx_hook(cub3d->screen.win, MLX_KEY_PRESS, (1L << 0), on_key_press, cub3d);
 	mlx_hook(cub3d->screen.win, MLX_KEY_RELEASE, (1L << 1),
-		on_key_release, &cub3d);
+		on_key_release, cub3d);
 }
