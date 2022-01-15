48972a1efcebbaed19698b8a133f9604c9612f36
Miguel Gueifao <miguel.gueifao@gmail.com>
2022-01-15 18:37:00 +0000

    Added prototype for get_dist and FOV constante

--- inc/c3d_graphics.h
+++ inc/c3d_graphics.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   c3d_general.h                                      :+:      :+:    :+:   */
+/*   c3d_graphics.h                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mgueifao <mgueifao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/16 06:06:29 by mgueifao          #+#    #+#             */
-/*   Updated: 2021/04/25 03:23:23 by mgueifao         ###   ########.fr       */
+/*   Updated: 2022/01/15 18:36:24 by mgueifao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,12 @@
 # define C3D_GRAPHICS_H
 
 # define NB_IMGS 2
+# define FOV 90.0
 
 void	my_mlx_pixel_put(t_img *img, int x, int y, int color);
 void	get_screen(t_screen *screen);
 void	next_frame(t_screen screen, void *mlx);
 void	render(t_cub3d cub3d);
+double	get_dist(double ang, t_game game, t_pos *pos);
 
 #endif
