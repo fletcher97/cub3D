ac857219392ed38185b39cab20545659fc1f4e0a
Miguel Gueifao <miguel.gueifao@gmail.com>
2022-01-15 18:32:18 +0000

    Added test function for debugging

--- src/graphics/render.c
+++ src/graphics/render.c
@@ -6,7 +6,7 @@
 /*   By: mgueifao <mgueifao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/09 00:21:57 by mgueifao          #+#    #+#             */
-/*   Updated: 2022/01/15 18:28:45 by mgueifao         ###   ########.fr       */
+/*   Updated: 2022/01/15 18:31:57 by mgueifao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,12 +57,25 @@ static void	render_floor(t_screen screen, int color, void *mlx)
 //	(void) screen;
 //}
 
+void test(t_cub3d c)
+{
+	t_pos pos;
+	// double dir = 46;
+	// double z = get_dist(((c.screen.width - 0) * FOV / c.screen.width) - FOV / 2.0, c.game, &pos);
+	double z = get_dist(-39.992679, c.game, &pos);
+	printf("dist: %lf\n", z);
+	printf("pos: %lf %lf\n", pos.x, pos.y);
+	exit(0);
+}
+
 void	render(t_cub3d cub3d)
 {
 	// static int b = 0;
 	// if (b)
 	// 	// while (1);
 	// 	return;
+
+	// test(cub3d);
 
 	render_ceil(cub3d.screen, 0x000000ff, cub3d.mlx);
 	render_floor(cub3d.screen, 0x00bbff55, cub3d.mlx);
