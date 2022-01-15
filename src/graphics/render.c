8abd0f13c653d2e09faff0d46a0c5d6023915e4f
Miguel Gueifao <miguel.gueifao@gmail.com>
2022-01-15 18:33:09 +0000

    Added render_wall [wip]

--- src/graphics/render.c
+++ src/graphics/render.c
@@ -6,7 +6,7 @@
 /*   By: mgueifao <mgueifao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/09 00:21:57 by mgueifao          #+#    #+#             */
-/*   Updated: 2022/01/15 18:31:57 by mgueifao         ###   ########.fr       */
+/*   Updated: 2022/01/15 18:32:42 by mgueifao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,12 +50,40 @@ static void	render_floor(t_screen screen, int color, void *mlx)
 	}
 }
 
-//static void	render_wall(t_player player, t_map map, t_screen screen)
-//{
-//	(void) player;
-//	(void) map;
-//	(void) screen;
-//}
+static void	render_wall(void *mlx, t_game game, t_screen screen)
+{
+	t_pos	pos;
+	int		c;
+	static int b = 0;
+
+	for (int i = 0; i < screen.width; i++)
+	{
+		double z = get_dist(((screen.width - i) * FOV / screen.width) - FOV / 2.0, game, &pos);
+		if (!z)
+			break ;
+		if (z < 0)
+			c = 0x66;
+		else
+			c = 0x22;
+		// double x = fabs(game.player.pos.x - pos.x);
+		// double y = fabs(game.player.pos.y - pos.y);
+		// printf("[%d] z: %lf\n", i, fabs(z));
+		// z = sqrt(x*x+y*y);
+		// printf("[%d] - z: %lf\n", i, fabs(z));
+		// printf("[%d] - z: %lf\n", i, tan(FOV/2) * fabs(z) / 2);
+		double vfovh = (tan(FOV / 2) * fabs(z)) / 2;
+		double pcount = (screen.height / 4) / vfovh;
+		// printf("pcount %lf color: %d\n", pcount, c);
+		int j = screen.height / 2 - pcount;
+		if (j < 0)
+			j = 0;
+		for (; j < screen.height / 2 + pcount && j < screen.height; j++)
+		{
+			my_mlx_pixel_put(&screen.img[1], i, j, mlx_get_color_value(mlx, c));
+		}
+	}
+	b = 1;
+}
 
 void test(t_cub3d c)
 {
