eae0a2a930bf2879c41adfda9efbc53af03de236
Miguel Gueifao <miguel.gueifao@gmail.com>
2022-01-15 18:30:17 +0000

    Added debug stop to prevent render to run multiple time

--- src/graphics/render.c
+++ src/graphics/render.c
@@ -6,7 +6,7 @@
 /*   By: mgueifao <mgueifao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/09 00:21:57 by mgueifao          #+#    #+#             */
-/*   Updated: 2022/01/14 00:24:37 by mgueifao         ###   ########.fr       */
+/*   Updated: 2022/01/15 18:28:45 by mgueifao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,7 +59,13 @@ static void	render_floor(t_screen screen, int color, void *mlx)
 
 void	render(t_cub3d cub3d)
 {
+	// static int b = 0;
+	// if (b)
+	// 	// while (1);
+	// 	return;
+
 	render_ceil(cub3d.screen, 0x000000ff, cub3d.mlx);
 	render_floor(cub3d.screen, 0x00bbff55, cub3d.mlx);
+	render_wall(cub3d.game.player, cub3d.game.map, cub3d.screen);
+	// b = 1;
 }
-//render_wall(cub3d.game.player, cub3d.game.map, cub3d.screen);
