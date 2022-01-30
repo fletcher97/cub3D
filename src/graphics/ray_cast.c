7f83a2b7fb533ecbb2592a7151df443ef9db18df
Miguel Gueifao <miguel.gueifao@gmail.com>
2022-01-30 21:13:19 +0000

    Added missing parentheses

--- src/graphics/ray_cast.c
+++ src/graphics/ray_cast.c
@@ -33,7 +33,7 @@
 
 double dist(double x1, double y1, double x2, double y2)
 {
-	return (sqrt(x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+	return (sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)));
 }
 
 double	h_check(t_game g, double ang)
