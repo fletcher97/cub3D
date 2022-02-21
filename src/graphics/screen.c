58ecdd2804b599eae9934a8b51656e9205bc0ba5
Miguel Gueifao <miguel.gueifao@gmail.com>
2022-02-21 09:32:51 +0000

    Norm fixes

--- src/graphics/screen.c
+++ src/graphics/screen.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   screen.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mgueifao <mgueifao@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fletcher <fletcher@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/06 18:55:16 by mgueifao          #+#    #+#             */
-/*   Updated: 2022/01/19 22:38:32 by mgueifao         ###   ########.fr       */
+/*   Updated: 2022/02/21 09:32:35 by fletcher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,6 @@ void	my_mlx_pixel_put(t_img *img, int x, int y, int color)
 	char	*dst;
 
 	dst = img->addr + (y * img->line + x * (img->bpp / 8));
-
 	dst[3] = color >> 24;
 	dst[2] = color >> 16;
 	dst[1] = color >> 8;
