7ac39cd01bf2360d53bfbec375226a3509644f5a
Joao Santos <ctw01895@criticaltechworks.com>
2022-02-23 18:57:00 +0000

    Player movement needs to be refactored.
    Just pasted the code that was working before trimming for the
    norminette. This one is working, but does not take into account the
    directions.

--- src/player/player.c
+++ src/player/player.c
@@ -16,6 +16,7 @@
 #include "c3d.h"
 #include "c3d_map.h"
 #include "c3d_player.h"
+#include <stdio.h>
 
 /*
  *	Moves the player if the movement keys (WASD) are being pressed. It checks if
@@ -27,30 +28,100 @@ void	move_player(t_game *game)
 {
 	double	x;
 	double	y;
-	int		temp_x;
-	int		temp_y;
 
 	x = game->player.pos.x;
 	y = game->player.pos.y;
-	if (game->player.x_mov)
+	if (game->player.x_mov | game->player.y_mov)
 	{
-		x += STEP * game->player.x_mov;
-		temp_x = (int) (x + game->player.x_mov * (CENTER_OFFSET - FLT_EPSILON));
-		if (game->map[(int) (y - CENTER_OFFSET + FLT_EPSILON)][temp_x] == '1'
-			|| game->map[(int) (y + CENTER_OFFSET + FLT_EPSILON)][temp_x] == '1')
-			x = ceil(x) - CENTER_OFFSET;
-	}
-	if (game->player.y_mov)
-	{
+		// Vertical mov
 		y += STEP * game->player.y_mov;
-		temp_y = (int) (y + game->player.y_mov * (CENTER_OFFSET - FLT_EPSILON));
-		if (game->map[temp_y][(int) (x - CENTER_OFFSET + FLT_EPSILON)] == '1'
-			|| game->map[temp_y][(int) (x + CENTER_OFFSET - FLT_EPSILON)] == '1')
-			y = ceil(y) - CENTER_OFFSET;
+
+
+		// Vertical adjustment
+		if (game->player.pos.y
+		< y)
+		{
+			if (game->map[(int) (y + CENTER_OFFSET - FLT_EPSILON)][(int) (x - CENTER_OFFSET + FLT_EPSILON)] == '1'
+				|| game->map[(int) (y + CENTER_OFFSET - FLT_EPSILON)][(int) (x + CENTER_OFFSET - FLT_EPSILON)] == '1')
+				y = ceil(y) - CENTER_OFFSET;
+		}
+		else if (game->player.pos.y> y)
+		{
+			if (game->map[(int) (y - CENTER_OFFSET + FLT_EPSILON)][(int) (x - CENTER_OFFSET + FLT_EPSILON)] == '1'
+				|| game->map[(int) (y - CENTER_OFFSET + FLT_EPSILON)][(int) (x + CENTER_OFFSET - FLT_EPSILON)] == '1')
+				y = ceil(y) - CENTER_OFFSET;
+		}
+
+		// HOrizontal mov
+		x += STEP * game->player.x_mov;
+
+		// Horizontal adjustment
+		if (game->player.pos.x
+		< x)
+		{
+			if (game->map[(int) (y + CENTER_OFFSET - FLT_EPSILON)][(int) (x + CENTER_OFFSET - FLT_EPSILON)] == '1'){
+				x = ceil(x) - CENTER_OFFSET;
+			}
+			else if (game->map[(int) (y - CENTER_OFFSET + FLT_EPSILON)][(int) (x + CENTER_OFFSET - FLT_EPSILON)] == '1'){
+				x = ceil(x) - CENTER_OFFSET;
+			}
+		}
+		else if (game->player.pos.x> x)
+		{
+			if (game->map[(int) (y + CENTER_OFFSET - FLT_EPSILON)][(int) (x - CENTER_OFFSET + FLT_EPSILON)] == '1'){
+				x = ceil(x) - CENTER_OFFSET;
+			}
+			else if (game->map[(int) (y - CENTER_OFFSET + FLT_EPSILON)][(int) (x - CENTER_OFFSET + FLT_EPSILON)] == '1'){
+				x = ceil(x) - CENTER_OFFSET;
+			}
+		}
+		// change_player_position(&game->map, y, x);
+		game->player.pos.x = x;
+		game->player.pos.y = y;
 	}
-	game->player.pos.x = x;
-	game->player.pos.y = y;
 }
+//void	move_player(t_game *game)
+//{
+//	double	x;
+//	double	y;
+//	int		temp_x;
+//	int		temp_y;
+//
+//	x = game->player.pos.x;
+//	y = game->player.pos.y;
+//	if (game->player.x_mov)
+//	{
+//		x += STEP * game->player.x_mov;
+////		x += STEP * game->player.x_mov * cos(game->player.dir * 180/M_PI);
+////		y += STEP * game->player.y_mov * sin(game->player.dir * 180/M_PI);
+////		printf("Degrees is %lf\n", game->player.dir * 180/M_PI);
+//		printf("Pos is: %lf, %lf\n", game->player.pos.x, game->player.pos.y);
+//		// x is a double, and hence cannot be used as an array subscript. So, we need to
+//		//cast it to an int. But that alone won't do it, since it will allow the player to
+//		//enter the walls.
+//		temp_x = (int) (x + game->player.x_mov * (CENTER_OFFSET - FLT_EPSILON));
+//		if (game->map[(int) (y - CENTER_OFFSET + FLT_EPSILON)][temp_x] == '1'
+//			|| game->map[(int) (y + CENTER_OFFSET + FLT_EPSILON)][temp_x] == '1')
+//			x = ceil(x) - CENTER_OFFSET;
+//	}
+//	if (game->player.y_mov)
+//	{
+//		y += STEP * game->player.y_mov;
+//		temp_y = (int) (y + game->player.y_mov * (CENTER_OFFSET - FLT_EPSILON));
+//		if (game->map[temp_y][(int) (x - CENTER_OFFSET + FLT_EPSILON)] == '1'
+//			|| game->map[temp_y][(int) (x + CENTER_OFFSET - FLT_EPSILON)] == '1')
+//			y = ceil(y) - CENTER_OFFSET;
+//	}
+//	game->player.pos.x = x;
+//	game->player.pos.y = y;
+//}
+/*
+x += STEP * game->player.x_mov * cos(game->player.dir * 180/M_PI);
+y += STEP * game->player.y_mov * sin(game->player.dir * 180/M_PI);
+printf("Degrees is %lf\n", game->player.dir * 180/M_PI);
+printf("Dir is %lf\n", game->player.dir);
+*/
+
 
 /*
  *	Moves the player if the movement keys (WASD) are being pressed. It checks if
