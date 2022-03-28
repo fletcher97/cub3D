8e0bae109ba5f27db3da7e23a41d555897d9b4b2
Joao Santos <ctw01895@criticaltechworks.com>
2022-03-28 17:51:15 +0100

    Erased unnecessary errors.

--- inc/errors.h
+++ inc/errors.h
@@ -23,11 +23,8 @@ typedef enum e_errors {
 	WRONG_EXTENSION				= -3,
 	ERROR_READING_MAP_FILE		= -4,
 	INVALID_MAP					= -5,
-	ERROR_READING_IMAGE_FILE	= -6,
-	NO_CONNECTION				= -7,
-	NO_WINDOW					= -8,
-	NO_IMG						= -9,
-	REPEATED_ASSET				= -10,
+	NO_IMG						= -6,
+	REPEATED_ASSET				= -7,
 }	t_errors;
 
 #endif //ERRORS_H
